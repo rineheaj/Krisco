fda6e67351bd9617d4b34f84e9db7ef0f39bbbe9
rineheaj <joshtrineheart@gmail.com>
2025-09-24 20:18:07 -0500

    file storage for render

---
+++ github_file_services.py
@@ -0,0 +1,37 @@
+from github import Github
+import os
+from datetime import datetime
+
+
+
+def save_guestbook_github(name, message):
+    g = Github(os.getenv["GITHUB_TOKEN"])
+    repo = g.get_repo(os.getenv["GITHUB_REPO"])
+    branch = os.environ.get("GITHUB_BRANCH", "main")
+
+    try:
+        contents = repo.get_contents("data/guestbook.txt", ref=branch)
+        old_text = contents.decoded_content.decode("utf-8")
+        sha = contents.sha
+    except Exception as e:
+        old_text = ""
+        sha = None
+        print(f"Error saving guestbook.txt file: {e}")
+    
+    entry = entry = f"{datetime.now().strftime('%m-%d-%Y %H:%M:%S')} | {name}: {message}\n"
+    new_text = old_text + entry
+
+    if sha:
+        repo.update_file(
+            path="data/guestbook.txt",
+            message=f"Add guestbook entry by {name}",
+            content=new_text,
+            sha=sha,
+            branch=branch,
+        )
+    else:
+        repo.create_file(
+            path="data/guestbook.txt",
+            message=f"Create guestbook.txt with first entry by {name}",
+            branch=branch,
+        )
